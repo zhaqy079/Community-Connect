1a201e0b067d99b48b10bfa379cb202c4ae5dc6e
Monica F <momonocode97@gmail.com>
2026-08-17 19:50:54 +0930

    wip: add initial Chat page

---
+++ app/chat/page.tsx
@@ -0,0 +1,9 @@
+export default function Chat() {
+    return (
+        <section className="flex flex-col items-center justify-center">
+            <h1 className="text-3xl">
+                chat here
+            </h1>
+        </section>
+    )
+}
