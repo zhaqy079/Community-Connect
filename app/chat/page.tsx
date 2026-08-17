deb594da44a44857a0f92be7e0914634bf83e8a1
Monica F <momonocode97@gmail.com>
2026-08-17 22:58:19 +0930

    wip: add prompt input field into Chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import UserInput from "@/components/ui/UserInput";
 
 type Message = {
     id: number;
@@ -36,29 +37,35 @@ export default function ChatPage() {
 
 
     return (
-        <section className="flex flex-col p-10 items-center justify-center">
+        <main className="flex h-[calc(100dvh-90px)] min-h-0 flex-col px-10 pt-6">
 
             {/* TODO: Add timestamp -> -- hh:mm dd/mm/yyyy -- */}
-
-            {/* TODO: Separate into two UI components later: 1. UserPromtBubble and 2. AssistantResponseBubble */}
-            {messages.map((message) => (
-                <div
-                    key={message.id}
-                    className={`relative break-words rounded-2xl mb-3 max-w-[80%]
+            <section className="flex-1 min-h-0 overflow-y-auto">
+                {/* TODO: Separate into two UI components later: 1. UserPromtBubble and 2. AssistantResponseBubble */}
+                {messages.map((message) => (
+                    <div
+                        key={message.id}
+                        className={`relative break-words rounded-2xl mb-3 max-w-[85%]
                         ${message.role === "user"
-                            ? "ml-auto bg-[var(--cc-teal)] p-4 text-white"
-                            : "mr-auto bg-gray-200 p-4"}
+                                ? "ml-auto bg-[var(--cc-teal)] p-4 text-white"
+                                : "mr-auto bg-gray-200 p-4"}
                     `}
-                >
-                    {message.content}
+                    >
+                        {message.content}
 
-                    {/* Icon to indicate response from Assistant */}
-                    {message.role === "assistant" && (
-                        <Image src="/logo.svg" alt="" width={30} height={30}
-                            className="absolute bottom-2 right-2" />
-                    )}
-                </div>
-            ))}
-        </section>
+                        {/* Icon to indicate response from Assistant */}
+                        {message.role === "assistant" && (
+                            <Image src="/logo.svg" alt="" width={30} height={30}
+                                className="absolute bottom-2 right-2" />
+                        )}
+                    </div>
+                ))}
+            </section>
+
+            {/* User prompt/input section */}
+            <UserInput />
+
+
+        </main>
     )
 }
