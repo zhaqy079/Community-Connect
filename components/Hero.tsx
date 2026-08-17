d545410c0bbf9d69c17e983d40c70d2888c11a76
Monica F <momonocode97@gmail.com>
2026-08-17 13:20:09 +0930

    feat: add input field and start chat btn on landing pg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,28 @@
 import Logo from "./ui/Logo"
+import ChatInput from "./ui/ChatInput"
 
 export default function Hero() {
+
   return (
     <section className="flex flex-col items-center justify-center">
 
-      <Logo/>
-
-      <h1 className="py-6 font-bold text-[var(--cc-teal)] text-3xl md:text-5xl lg:text-6xl">
+            <h1 className="py-6 font-bold text-[var(--cc-teal)] text-3xl md:text-5xl lg:text-6xl">
         Community Connect
       </h1>
 
-      <p className="text-center max-w-2xl text-lg text-[var(--cc-text-light)]">
+      <Logo/>
+
+
+      <p className="max-w-2xl text-lg pt-6 text-[var(--cc-text-light)]">
         Helping people discover local services, support, and community
         resources in one accessible place.
       </p>
+
+      <p className="pt-6 text-center font-bold max-w-2xl text-lg text-[var(--cc-text-light)]">
+        Describe what you're looking for and we'll help you find the right support.
+      </p>
+
+      <ChatInput/>
 
     </section>
   )
