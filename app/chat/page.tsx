8b4ef6107b1f498878203bc1ccde0348ba719ba9
Monica F <momonocode97@gmail.com>
2026-08-26 21:43:23 +0930

    feat: render basic Service info fetched from getServicesByCategory

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import UserInput from "@/components/ui/UserInput";
 
-import { type ServiceCategory } from "@/src/types/service";
+import { type ServiceCategory, Service } from "@/src/types/service";
 import { getServicesByCategory } from "@/src/lib/services";
 
 type InitialChatData = {
@@ -16,6 +16,7 @@ type Message = {
     id: number;
     role: "user" | "assistant";
     content: string;
+    services?: Service[];
 };
 
 export default function ChatPage() {
@@ -29,12 +30,13 @@ export default function ChatPage() {
 
         const initialChatData: InitialChatData = JSON.parse(storedData);
 
-        const mathcedServices = initialChatData.categoryId
+        const matchedServices = initialChatData.categoryId
             ? getServicesByCategory(initialChatData.categoryId) : [];
 
-        console.log(mathcedServices);
+        console.log(matchedServices);
 
-        setMessages([
+        // Set initial meesages and corresponding responses.
+        const initialMessages: Message[] = [
             {
                 id: 1,
                 role: "user",
@@ -43,11 +45,14 @@ export default function ChatPage() {
             {
                 id: 2,
                 role: "assistant",
-                content: mathcedServices.length > 0
-                    ? `Thanks for sharing that! I have found ${mathcedServices.length} services in this support area.`
-                    : "Thanks for sharing that. Tell me a little more about the support you need",
+                content: matchedServices.length > 0
+                    ? `Thank you for sharing the information. I found ${matchedServices.length} services that may be of help.`
+                    : "Thanks for sharing that. Can you please tell me a little bit more about the support you need?",
+                services: matchedServices.length > 0 ? matchedServices : undefined,
             }
-        ])
+        ];
+
+        setMessages(initialMessages);
 
         sessionStorage.removeItem("initialChatData")
     }, []);
@@ -69,6 +74,26 @@ export default function ChatPage() {
                     `}
                     >
                         {message.content}
+
+                        {message.services && message.services.length > 0 && (
+                            <div className="mt-3 mr-auto w-full max-w-2xl space-y-3 mb-8">
+                                {/* TODO: Design a reusable expandable UI card component to display Service details */}
+                                {message.services.map((service) => (
+                                    <div
+                                        key={service.id}
+                                        className="rounded-2xl border border-gray-200 bg-white p-5 shadow-sm"
+                                    >
+                                        <h2 className="font-semibold text-gray-900">
+                                            {service.name}
+                                        </h2>
+
+                                        <p className="mt-2 text-sm text-gray-600">
+                                            {service.shortDescription}
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
 
                         {/* Icon to indicate response from Assistant */}
                         {message.role === "assistant" && (
