8991b35a8193ffd0630849cb2f6bf560b486e42f
Qianting <zhaqy079@mymail.unisa.edu.au>
2026-08-28 23:13:09 +0930

    Update Home page layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,15 +4,15 @@ import ChatInput from "./ui/ChatInput"
 export default function Hero() {
 
   return (
-    <section className="flex flex-col items-center justify-center">
+    <section className="flex flex-col items-center justify-center px-4">
 
-      <h1 className="pt-8 pb-6 font-bold text-[var(--cc-teal)] text-3xl md:text-5xl lg:text-6xl">
+      <h1 className="pt-8 pb-6 font-bold text-center  text-[var(--cc-teal)] text-3xl md:text-5xl lg:text-6xl">
         Community Connect
       </h1>
 
       <Logo />
 
-      <p className="max-w-2xl text-lg pt-6 text-[var(--cc-text-light)]">
+      <p className="max-w-2xl text-lg pt-6 text-center text-[var(--cc-text-light)] leading-relaxed sm:text-xl">
         {/* Helping people discover local services, support, and community
         resources in one accessible place. */}
         Find South Australian support in one accessible place.
